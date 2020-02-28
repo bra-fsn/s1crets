cd2b5ba07b726927b5a184d91b02d777bf71b1a1
bra@fsn.hu <bra@fsn.hu>
2020-02-28 13:22:33 +0100

    New methods

--- s1crets/cli.py
+++ s1crets/cli.py
@@ -12,8 +12,19 @@ def main():
 @click.option('--provider', help='Secrets provider', default='aws.sm',
               show_default=True, type=click.Choice(['aws.sm', 'aws.ps']))
 @click.argument('path', nargs=1, required=True)
-def get(provider, path):
-    print(s1crets.get(provider, path))
+@click.argument('keypath', nargs=-1, type=click.UNPROCESSED)
+def get(provider, path, keypath):
+    click.echo(s1crets.get(provider, path, keypath=keypath))
+
+
+@main.command()
+@click.option('--provider', help='Secrets provider', default='aws.sm',
+              show_default=True, type=click.Choice(['aws.sm', 'aws.ps']))
+@click.argument('path', nargs=1, required=True)
+@click.argument('keypath', nargs=-1, type=click.UNPROCESSED)
+def exists(provider, path, keypath):
+    if not s1crets.path_exists(provider, path, keypath=keypath):
+        sys.exit(1)
 
 
 @main.command()
@@ -23,3 +34,12 @@ def get(provider, path):
 def get_by_path(provider, path):
     for k, v in s1crets.get_by_path(provider, path).items():
         print(k, v)
+
+
+@main.command()
+@click.option('--provider', help='Secrets provider', default='aws.sm',
+              show_default=True, type=click.Choice(['aws.sm', 'aws.ps']))
+@click.argument('path', nargs=1, required=True)
+@click.argument('value', nargs=1, required=True)
+def update(provider, path, value):
+    s1crets.update(provider, path, value)
